6e81b1bce616f34f6f11ef0e497ff03d7babafbe
sunilsd7 <sumildumre555@gmail.com>
2025-02-19 22:13:10 +0545

    Initial commit

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,62 +1,62 @@
-import React, { useState } from "react";
-import questions from "../data/questions";
-import Question from "./Question";
-import Result from "./Result";
-import button from '../ComponentUI/Button';
+  import React, { useState } from "react";
+  import questions from "../data/questions";
+  import Question from "./Question";
+  import Result from "./Result";
 
-function Quiz() {
-  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-  const [score, setScore] = useState(0);
-  const [gameOver, setGameOver] = useState(false);
-  const [submit,setsubmit]=useState(true);
 
-  const handleAnswer = (selectedOption) => {
-    if (selectedOption === questions[currentQuestionIndex].answer) {
-      setScore(score + 1);
-    }else(
-      setScore(score -0.5)
-    )
-   
-    const nextIndex = currentQuestionIndex + 1;
-    if (nextIndex < questions.length) {
-      setCurrentQuestionIndex(nextIndex);
-    } else {
-      setGameOver(true);
-      setsubmit(false)
-    }
-  };
+  function Quiz() {
+    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+    const [score, setScore] = useState(0);
+    const [gameOver, setGameOver] = useState(false);
+    const [setsubmit]=useState(true);
 
-  const handleSubmit =(e) =>{
-    e.preventDefault();
-  
-    if(handleSubmit){
+    const handleAnswer = (selectedOption) => {
+      if (selectedOption === questions[currentQuestionIndex].answer) {
+        setScore(score + 1);
+      }else(
+        setScore(score -0.5)
+      )
     
-      setGameOver(true);
-      setsubmit(false);
+      const nextIndex = currentQuestionIndex + 1;
+      if (nextIndex < questions.length) {
+        setCurrentQuestionIndex(nextIndex);
+      } else {
+        setGameOver(true);
+        setsubmit(false)
+      }
+    };
+
+    const handleSubmit =(e) =>{
+      e.preventDefault();
+    
+      if(handleSubmit){
+      
+        setGameOver(true);
+        setsubmit(false);
+      }
     }
+
+    return (
+      <div className="grid w-100 mx-auto items-center mt-10">
+        {gameOver ? (
+          <Result score={score} total={currentQuestionIndex} />
+        ) : (
+          <>
+          <Question
+            question={questions[currentQuestionIndex]}
+            onAnswer={handleAnswer}
+          
+          />
+          <button onClick={handleSubmit} className="bg-blue-400 rounded-lg py-4">Submit</button>
+          
+
+          </>
+        )}
+          
+
+        
+      </div>
+    );
   }
 
-  return (
-    <div className="grid w-100 mx-auto items-center mt-10">
-      {gameOver ? (
-        <Result score={score} total={questions.length} />
-      ) : (
-        <>
-        <Question
-          question={questions[currentQuestionIndex]}
-          onAnswer={handleAnswer}
-         
-        />
-        <button onClick={handleSubmit} className="bg-blue-400 rounded-lg py-4">Submit</button>
-        
-
-        </>
-      )}
-        
-
-      
-    </div>
-  );
-}
-
-export default Quiz;
+  export default Quiz;
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-
-
-
-
 const Home = () => {
   return (
     <div className=' p-32 justify-center'>
